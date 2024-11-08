dface23e0dca2556128ebb053a5ccfa2b89cdb0a
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-11-08 13:07:20 +0100

    ref: renamed playwright artifacts folder
    Renamed the output directory where playwright artifacts are stored from `test-results` to `playwright-test-artifacts`.

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
   // If a test fails, retry it additional 2 times
   retries: 2,
   // Artifacts folder where screenshots, videos, and traces are stored.
-  outputDir: 'test-results/',
+  outputDir: 'playwright-test-artifacts/',
 
   // Run your local dev server before starting the tests:
   // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
