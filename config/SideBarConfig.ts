106511884b0402652690e56828d4604daf1cfe0d
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-11-09 12:15:46 +0100

    feat: created SideBarConfig parameter module
    This module features configurations and parameters related to the SideBar.

---
+++ config/SideBarConfig.ts
@@ -0,0 +1,22 @@
+import SideBarProps from '@/typings/root/SideBarProps'
+
+export const SideBarConfiguration: SideBarProps = {
+  title: 'Power Plant Operations',
+  icon: 'icon',
+  items: [
+    {
+      title: 'Item 1',
+      href: '/item1',
+      icon: 'icon',
+    },
+    {
+      title: 'Item 2',
+      href: '/item2',
+      icon: 'icon',
+    },
+  ],
+}
+
+export const DesktopSideBarVisibilityBreakpoints = 'hidden md:flex'
+
+export const MobileSideBarVisibilityBreakpoints = 'flex md:hidden'
