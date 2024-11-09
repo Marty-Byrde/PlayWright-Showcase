99af040fafd87a8fbe11f5808145471c507f594e
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-11-09 12:22:36 +0100

    feat: drafted SideBar component
    Currently exposes a draft of a SideBar component that wraps a Desktop and Mobile Sidebar inside of it. Depending on the viewports-width and the settings in the SideBarConfig file the MobileSidebar will be display while the DesktopSideBar is hidden and vice versa.

---
+++ components/root/SideBar.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import SideBarProps from '@/typings/root/SideBarProps'
+import PowerIcon from '@/icons/PowerIcon'
+import { twMerge } from 'tailwind-merge'
+import { DesktopSideBarVisibilityBreakpoints, MobileSideBarVisibilityBreakpoints, SideBarConfiguration } from '@/config/SideBarConfig'
+
+export default async function SideBar() {
+  return (
+    <>
+      <MobileSideBar {...SideBarConfiguration} />
+      <DesktopSideBar {...SideBarConfiguration} />
+    </>
+  )
+}
+
+function MobileSideBar(props: SideBarProps) {
+  return (
+    <div className={twMerge('bg-gray-50 dark:bg-neutral-900 p-4 border-b-2 justify-between', MobileSideBarVisibilityBreakpoints)}>
+      <div id={'sidebar-header'} className='flex gap-4 items-center justify-center'>
+        <PowerIcon />
+        <h3 className='text-xl'>{props.title}</h3>
+      </div>
+
+      <>Hamburger</>
+    </div>
+  )
+}
+
+function DesktopSideBar(props: SideBarProps) {
+  return (
+    <div className={twMerge('fixed w-[350px] dark:bg-neutral-900 bg-gray-50 h-full flex-col p-3 pl-2', DesktopSideBarVisibilityBreakpoints)}>
+      <div id={'sidebar-header'} className='flex gap-4 items-center justify-center pb-3 border-b-2'>
+        <PowerIcon />
+        <h3 className='text-2xl'>{props.title}</h3>
+      </div>
+
+      <div className='h-full bg-gray-800'>Sidebar Body</div>
+
+      <div className='pt-3 mx-auto'>Sidebar Footer</div>
+    </div>
+  )
+}
