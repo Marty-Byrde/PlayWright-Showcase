6e41a2437fb49ebd9aba22f8b87419f5a812fb3c
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-11-09 12:02:15 +0100

    chore: improved type-safety of tailwind.config
    By declaring and initializing a variable of type (partial) Config we can set valid properties. At the same the type-safety analysis is more transparent.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from 'tailwindcss'
 
-export default {
+const config: Partial<Config> = {
   content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
@@ -11,4 +11,6 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config
+}
+
+export default config
