c205615a402e4697e387470455bc726fa24f3630
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-11-09 13:10:12 +0100

    tests: updated root-page tests
    Adjusted the playwright tests, since the template code has been removed.

--- tests/playwright/root/page.test.ts
+++ tests/playwright/root/page.test.ts
@@ -2,14 +2,7 @@ import { expect, test } from './fixtures'
 
 test('Root Page has heading', async ({ page }) => {
   await page.goto('http://localhost:3000/')
-  const heading = page.getByRole('heading', { name: 'Home' })
+  const heading = page.getByRole('heading', { name: 'Root Page..' })
 
   await expect(heading).toBeVisible()
-})
-
-test('Root Page has Link', async ({ page }) => {
-  await page.goto('http://localhost:3000')
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('link', { name: 'Go to nextjs.org' })).toBeVisible()
 })
