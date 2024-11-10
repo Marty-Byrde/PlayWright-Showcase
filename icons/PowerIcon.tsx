40a94e04efe9a30367eed29eba6855e807a8e1e7
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-11-10 09:05:53 +0100

    ref: removed async from signature in PowerIcon
    The PowerIcon is now no longer asynchronous based on its signature. This change was necessary because Jest is unable to test a component (async or not) that uses async components.

--- icons/PowerIcon.tsx
+++ icons/PowerIcon.tsx
@@ -5,7 +5,7 @@ interface PowerPlantIconProps extends IconProps {
   strokeWidth?: number
 }
 
-export default async function PowerIcon(props: PowerPlantIconProps) {
+export default function PowerIcon(props: PowerPlantIconProps) {
   return (
     <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={props.strokeWidth ?? 1.5} stroke='currentColor' className={twMerge('size-6', props.className)}>
       <path strokeLinecap='round' strokeLinejoin='round' d='m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z' />
