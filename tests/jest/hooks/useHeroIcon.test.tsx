d5c7f85948723629ac8f8e9b58ba8d076b165c05
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-11-12 20:43:01 +0100

    tests: added parameterized useHeroIcon type test
    The test now checks whether an icon is rendered correctly based on the type.

--- tests/jest/hooks/useHeroIcon.test.tsx
+++ tests/jest/hooks/useHeroIcon.test.tsx
@@ -1,6 +1,7 @@
 import { describe, it } from '@jest/globals'
 import { render } from '@testing-library/react'
 import useHeroIcon from '@/hooks/useHeroIcon'
+import each from 'jest-each'
 
 describe('useHeroIcon', () => {
   it('renders an Icon corrrectly', async () => {
@@ -22,5 +23,15 @@ describe('useHeroIcon', () => {
     expect(svgs).toHaveLength(1)
     expect(svgs[0]).toBeInTheDocument()
     expect(svgs[0]).toHaveClass('size-12')
+  })
+
+  each([['outline'], ['solid'], [undefined]]).it('renders an Icon with optional type correctly', async (type) => {
+    const Icon = useHeroIcon({ iconName: 'HomeIcon', type })
+    const { container } = render(<Icon />)
+
+    const svgs = container.getElementsByTagName('svg')
+
+    expect(svgs).toHaveLength(1)
+    expect(svgs[0]).toBeInTheDocument()
   })
 })
