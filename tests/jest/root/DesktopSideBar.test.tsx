43d80d448287b43e09311851b2c51f696a8f4cac
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-11-15 18:14:47 +0100

    feat: updated DesktopSideBar test case
    The DesktopSideBar test case that checks whether all SideBar items are correctly displayed has been adapted. The reason for this is that with the updated design of the `RenderSideBarItem` component sub-items are no longer displayed without opening the Disclosure Panel.

--- tests/jest/root/DesktopSideBar.test.tsx
+++ tests/jest/root/DesktopSideBar.test.tsx
@@ -1,7 +1,11 @@
-import { render, screen } from '@testing-library/react'
+import { act, render, screen } from '@testing-library/react'
 import { describe, it } from '@jest/globals'
 import { SideBarConfiguration } from '@/config/SideBarConfig'
 import DesktopSideBar from '@/components/root/SideBar/variants/DesktopSideBar'
+import { wait } from '@testing-library/user-event/dist/utils'
+import { mockAnimationsApi } from 'jsdom-testing-mocks'
+
+mockAnimationsApi()
 
 describe('DesktopSideBar', () => {
   it('has the correct title', async () => {
@@ -14,16 +18,49 @@ describe('DesktopSideBar', () => {
     expect(sidebarHeading).toHaveTextContent(SideBarConfiguration.title)
   })
 
-  it('shows navigation items', async () => {
+  it('shows navigation items and containers (=item with subitems)', async () => {
     const Component = await DesktopSideBar({ ...SideBarConfiguration })
     render(Component)
 
-    const sidebarItems = screen.getAllByRole('listitem', {})
-    const sidebarItemTitles = sidebarItems.map((item) => item.textContent)
+    const initialSidebarItems = screen.getAllByRole('listitem', {})
+    await act(() => openSideBarElementDisclosures(initialSidebarItems))
+
+    //* After opening all disclosures, all items should be visible
+    const items = screen.getAllByRole('listitem', {})
+    const itemTitles = items.map((item) => item.getElementsByClassName('element-title')[0].textContent)
 
     const expectedItems = SideBarConfiguration.items.reduce((acc, item) => (item.items ? acc.concat(item).concat(item.items) : acc.concat(item)), [])
 
-    expect(sidebarItems).toHaveLength(expectedItems.length)
-    expect(sidebarItemTitles).toEqual(expectedItems.map((item) => item.title))
+    expect(items).toHaveLength(expectedItems.length)
+    expect(itemTitles).toEqual(expectedItems.map((item) => item.title))
   })
 })
+
+/**
+ * Opens all disclosures in the sidebar recursively.
+ * Note that by using the act function, failing the test when the disclosure does not open is not possible.
+ * Interestingly, using the act function throws an error when calling the function recursively.
+ * @returns Promise<void> - opens all disclosures when using act, even though it is not called recursively
+ * @param elements
+ */
+async function openSideBarElementDisclosures(elements: HTMLElement[]) {
+  for (const element of elements) {
+    //* Disclosure-Open button
+    if (element.children[0].tagName === 'BUTTON') {
+      const button = element.children[0] as HTMLButtonElement
+      button.click()
+      await wait(500) // wait for animation to finish
+
+      //? Disclosure-Panel did not open
+      if (element.children.length === 1) {
+        // fail('Disclosure-Panel did not open')
+      }
+
+      //* Recursive call for subitems
+      // await openSideBarElementDisclosures(element.children[1].children as HTMLElement[])
+    }
+
+    //* Link of simple element (no subitems)
+    if (element.children[0].tagName === 'A') continue
+  }
+}
