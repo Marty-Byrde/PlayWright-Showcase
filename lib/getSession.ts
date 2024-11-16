9caf2d245c2eab11dd2fa616925fe09b02311ead
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-11-16 16:43:15 +0100

    feat: created getSession helper-module
    This module will be used on the server-side to get the current session user, when there is a session.

---
+++ lib/getSession.ts
@@ -0,0 +1,18 @@
+'use server'
+import { getServerSession, User } from 'next-auth'
+import { options } from '@/app/api/auth/[...nextauth]/options'
+
+interface GitHubUser extends User {
+  name: string
+  image: string
+  email: string
+}
+
+export default async function getSession() {
+  const session = await getServerSession(options)
+  if (!session || !session.user) return { user: null }
+
+  const user = session.user as GitHubUser
+
+  return { user }
+}
