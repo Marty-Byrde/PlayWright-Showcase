11485b046b31e260890051612683008906bc78a8
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-11-16 11:46:32 +0100

    feat: created MobileSideBarDialog component
    This component is used to animate the appearance of navigation-items for mobile-devices. The children that passed on the component are then displayed within this sliding-container.

--- components/root/SideBar/variants/MobileSideBar.tsx
+++ components/root/SideBar/variants/MobileSideBar.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/jsx-max-depth */
 'use client'
 
-import { createContext, useContext, useState } from 'react'
+import { createContext, Fragment, useContext, useState } from 'react'
 import { twMerge } from 'tailwind-merge'
 import { MobileSideBarVisibilityBreakpoints } from '@/config/SideBarConfig'
 import { Bars3Icon, MoonIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import SideBarProps from '@/typings/root/SideBarProps'
 import useHeroIcon from '@/hooks/useHeroIcon'
+import { Dialog, Transition } from '@headlessui/react'
 
 interface MobileSideBarContextProps {
   isOpen: boolean
@@ -49,5 +50,43 @@ function OpenCloseButton() {
 
       <ButtonIcon className='size-6' />
     </button>
+  )
+}
+
+/**
+ * Renders the dialog that slides in from the left and displays renders the provided children in it
+ * @param children The content / children that are rendered in the dialog
+ */
+function MobileSideBarDialog({ children }: { children: React.ReactNode }) {
+  const { isOpen, setIsOpen } = useMobileSideBarContext()
+
+  return (
+    <Transition.Root show={isOpen} as={Fragment}>
+      <Dialog as='div' className='relative z-50 lg:hidden' onClose={setIsOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter='transition-opacity ease-linear duration-300'
+          enterFrom='opacity-0'
+          enterTo='opacity-100'
+          leave='transition-opacity ease-linear duration-300'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'>
+          <div className='fixed inset-0 bg-neutral-900/80' />
+        </Transition.Child>
+
+        <div className='fixed inset-0 flex'>
+          <Transition.Child
+            as={Fragment}
+            enter='transition ease-in-out duration-300 transform'
+            enterFrom='-translate-x-full'
+            enterTo='translate-x-0'
+            leave='transition ease-in-out duration-300 transform'
+            leaveFrom='translate-x-0'
+            leaveTo='-translate-x-full'>
+            <Dialog.Panel className='relative flex w-full max-w-xs flex-1 sm:max-w-sm'>{children}</Dialog.Panel>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition.Root>
   )
 }
