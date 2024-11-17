940baf635f9ea52efc07b77897ffe96d1b279a52
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-11-17 16:59:43 +0100

    ref: renamed provider environment variables
    The reason for this that environment-variables on GitHub must not start with 'GITHUB_*'.

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,15 +1,15 @@
 import type { NextAuthOptions } from 'next-auth'
 import GitHubProvider from 'next-auth/providers/github'
 
-const { GITHUB_ID, GITHUB_SECRET } = process.env
+const { NEXT_AUTH_GITHUB_ID, NEXT_AUTH_GITHUB_SECRET } = process.env
 
-if (!GITHUB_ID || !GITHUB_SECRET) throw new Error('Missing Github Credentials...')
+if (!NEXT_AUTH_GITHUB_ID || !NEXT_AUTH_GITHUB_SECRET) throw new Error('Missing Github Credentials...')
 
 export const options: NextAuthOptions = {
   providers: [
     GitHubProvider({
-      clientId: GITHUB_ID,
-      clientSecret: GITHUB_SECRET,
+      clientId: NEXT_AUTH_GITHUB_ID,
+      clientSecret: NEXT_AUTH_GITHUB_SECRET,
       name: 'Github-Provider',
     }),
   ],
