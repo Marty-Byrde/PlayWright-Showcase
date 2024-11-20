2177dacf27d127fa2d91af2f11deb1ba6a05f0db
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-11-20 15:19:22 +0100

    feat: created createDummyPowerPlant function
    This function creates a given amount of dummy PowerPlant objects. This function is needed to simulate the backend that is not yet available.

--- typings/Shared/PowerPlant.ts
+++ typings/Shared/PowerPlant.ts
@@ -5,3 +5,13 @@ export default interface PowerPlantProps {
   metric2: number
   metric3: number
 }
+
+export function createDummyPowerPlants(amount: number): PowerPlantProps[] {
+  return Array.from({ length: amount }).map(() => ({
+    name: 'Dummy Power Plant',
+    id: +(Math.random() * 100).toFixed(0),
+    capacity: +Math.random().toFixed(2),
+    metric2: +Math.random().toFixed(2),
+    metric3: +Math.random().toFixed(2),
+  }))
+}
