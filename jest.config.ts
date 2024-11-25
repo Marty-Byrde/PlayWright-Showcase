7bd2c6058bb6dd161689ee60b92764b6587c0b08
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-11-25 10:55:04 +0100

    chore: added named-import-paths to jest-config
    Now it is possible to mock entities based on their named path, like '@/lib/getSession' instead of providing the absolute path to the entity.

--- jest.config.ts
+++ jest.config.ts
@@ -14,6 +14,11 @@ const config: Config = {
   testEnvironment: 'jsdom',
   // Add more setup options before each test is run
   setupFilesAfterEnv: ['./jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^@lib/(.*)$': '<rootDir>/lib/$1',
+    '^@components/(.*)$': '<rootDir>/components/$1',
+  },
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
