8b54dda13cf1c55936706967e327918c3712429e
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-11-30 10:56:12 +0100

    feat: added test suite for key-findings showcase

---
+++ tests/playwright/Test-Case-Highlights.ts
@@ -0,0 +1,42 @@
+import { test, ViewportSize } from '@playwright/test'
+import { Breakpoints } from '@/tests/helpers/TailwindConfig'
+import { describe } from 'node:test'
+
+const isDesktop = ({ width }: ViewportSize | null) => width > Breakpoints['md']
+const isMobile = ({ width }: ViewportSize | null) => width <= Breakpoints['md']
+
+//? Define Fixures for Test Suite (optional)
+test.use({
+  colorScheme: 'light',
+  // storageState: 'state.json', //? provide cookies, local storage, etc.
+})
+
+//? Exemplary Test Suite Structuring
+describe('SideBar Navigation', () => {
+  test('- test navigation functionality on various devices', async ({ page, viewport }) => {
+    await test.step('Navigate to the root page', async () => {
+      await page.goto('http://localhost:3000/')
+      await page.waitForSelector('h1')
+    })
+
+    await test.step('Interact with Navigation Bar on larger screens', async () => {
+      test.skip(isMobile(viewport), `Skipping step because this step works only for large (desktop) screens`)
+
+      //? provide snapshots for later use (also event based, e.g. on error)
+      await test.info().attach('screenshot', {
+        body: await page.screenshot(),
+        contentType: 'image/png',
+      })
+
+      //? ... Tests and assertions
+    })
+
+    await test.step('Interact with Navigation Bar on smaller screens', async () => {
+      test.skip(isDesktop(viewport), `Skipping step because this step works only for small (mobile) screens`)
+
+      //? ... Tests and assertions
+    })
+
+    //? ... More tests that work again on both large and small screens
+  })
+})
